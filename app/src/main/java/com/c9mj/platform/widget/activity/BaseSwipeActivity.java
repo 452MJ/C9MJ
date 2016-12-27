77e4a92fb16aab6154be9cc276669d3e540d9149
452MJ <447180005@qq.com>
2016-12-27 19:17:51 +0800

    优化代码.

--- app/src/main/java/com/c9mj/platform/widget/activity/BaseSwipeActivity.java
+++ app/src/main/java/com/c9mj/platform/widget/activity/BaseSwipeActivity.java
@@ -1,15 +1,8 @@
 package com.c9mj.platform.widget.activity;
 
-import android.Manifest;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
-import com.c9mj.platform.R;
-import com.c9mj.platform.util.ToastUtil;
-import com.tbruyelle.rxpermissions2.RxPermissions;
-
-import io.reactivex.functions.Consumer;
 import me.yokeyword.fragmentation_swipeback.SwipeBackActivity;
 
 /**
@@ -31,27 +24,27 @@ public class BaseSwipeActivity extends SwipeBackActivity {
 //        }
 
         //6.0权限申请
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            RxPermissions rxPermissions = RxPermissions.getInstance(this);
-            rxPermissions
-                    .request(
-                            Manifest.permission.CAMERA,
-                            Manifest.permission.INTERNET,
-                            Manifest.permission.READ_EXTERNAL_STORAGE,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    .subscribe(new Consumer<Boolean>() {
-                        @Override
-                        public void accept(Boolean granted) throws Exception {
-                            if (granted) {
-                                //所有权限被允许
-                            } else {
-                                //至少一个权限被拒绝
-                                ToastUtil.show(getString(R.string.error_grant));
-                            }
-                        }
-
-                    });
-        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            RxPermissions rxPermissions = new RxPermissions(this);
+//            rxPermissions
+//                    .request(
+//                            Manifest.permission.CAMERA,
+//                            Manifest.permission.INTERNET,
+//                            Manifest.permission.READ_EXTERNAL_STORAGE,
+//                            Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//                    .subscribe(new Consumer<Boolean>() {
+//                        @Override
+//                        public void accept(Boolean granted) throws Exception {
+//                            if (granted) {
+//                                //所有权限被允许
+//                            } else {
+//                                //至少一个权限被拒绝
+//                                ToastUtil.show(getString(R.string.error_grant));
+//                            }
+//                        }
+//
+//                    });
+//        }
 
     }
 }
